d32ad12ea1ac13cc1922d8a8bec655a5127a4bd0
EmmaGrande <emajulio.ej@gmail.com>
2020-04-27 15:10:54 -0300

    Primer comit con toda la estructura en blanco

---
+++ ProyectoVentaOnLine/CNegocio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CNegocio
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProyectoVentaOnLine/Datos/Class1.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Datos
+{
+    public class Class1
+    {
+        public void cosa()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
